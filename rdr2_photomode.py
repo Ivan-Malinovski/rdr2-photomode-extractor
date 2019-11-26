bd371f2b78c8e10866617543d33ecffa3e1caf63
Ivan Malinovski <ivan@malinov.ski>
2019-11-26 15:29:03 +0100

    Added functionality
    Added custom RDR2 save-file directory and custom output folder

--- rdr2_photomode.py
+++ rdr2_photomode.py
@@ -1,8 +1,21 @@
 import os
 import errno
 
-# You might need to adjust this path depending on your locale
-dir = os.path.expanduser('~') + "\Documents\Rockstar Games\Red Dead Redemption 2\Profiles"
+if os.path.isfile('./rdr2dir.txt'):
+    f = open("rdr2dir.txt", "r")
+    rdr_dir = f.readlines()[0]
+    if len(rdr_dir.strip()) == 0:
+        rdr_dir = os.path.expanduser('~') + "\Documents\Rockstar Games\Red Dead Redemption 2\Profiles"
+else:
+    rdr_dir = os.path.expanduser('~') + "\Documents\Rockstar Games\Red Dead Redemption 2\Profiles"
+
+if os.path.isfile('./photodir.txt'):
+    f = open("photodir.txt", "r")
+    photo_dir = f.readlines()[0]
+    if len(photo_dir.strip()) == 0:
+        photo_dir = "Images\\"
+else:
+    photo_dir = "Images\\"
 
 def mkdir_p(path):
     try:
@@ -15,13 +28,13 @@ def mkdir_p(path):
 
 def convert(name, path):
     with open(path + "/" + name, 'rb') as in_file:
-        with open("Images/" +name + '.jpg', 'wb') as out_file:
+        with open(photo_dir +name + '.jpg', 'wb') as out_file:
             out_file.write(in_file.read()[300:])
 
-mkdir_p( "Images" )
+mkdir_p(photo_dir)
 
-for folderName in os.listdir(dir):
-        folder = dir + "\\" + folderName
+for folderName in os.listdir(rdr_dir):
+        folder = rdr_dir + "\\" + folderName
         for filename in os.listdir(folder):
                 if(filename.startswith( 'PRDR' )):
                         convert(filename, folder)
